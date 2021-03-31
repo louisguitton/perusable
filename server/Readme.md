ff577d7248395e4a570d9e862dd214885d574c97
Louis Guitton <louisguitton93@gmail.com>
2021-03-31 11:06:10 +0200

    Initial commit - Part 1 + exo 1
    https://testdriven.io/courses/django-full-text-search/part-one-next-steps/

---
+++ server/Readme.md
@@ -0,0 +1,12 @@
+# server
+
+```sh
+# create a project called perusable
+django-admin startproject perusable .
+# create an app called catalog
+python manage.py startapp catalog
+```
+
+> Projects vs. apps
+What’s the difference between a project and an app? An app is a Web application that does something – e.g., a Weblog system, a database of public records or a small poll app. A project is a collection of configuration and apps for a particular website. A project can contain multiple apps. An app can be in multiple projects.
+Ref: https://docs.djangoproject.com/en/3.1/intro/tutorial01/#creating-a-project
---
+++ server/catalog/models.py
@@ -0,0 +1,26 @@
+import uuid
+
+from django.db import models
+from django.contrib.postgres import indexes
+
+class Wine(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    country = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    points = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    variety = models.CharField(max_length=255)
+    winery = models.CharField(max_length=255)
+    thumbnail = models.ImageField(null=True, upload_to='wines')
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        indexes = [
+            indexes.GinIndex(
+                name='review_author_ln_gin_idx', 
+                fields=['last_name'], 
+                opclasses=['gin_trgm_ops'],
+            )
+        ]
